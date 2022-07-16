627d4e82f06bd0a7b87e01b8f38eb628c18b5827
pryority <matthewapryor@gmail.com>
2022-07-16 11:58:29 -0400

    UI renders if the state has fetched weather data from OpenWeather

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Main from './components/Main'
 function App() {
   const [lat, setLat] = useState([]);
   const [long, setLong] = useState([]);
-  const [data, setData] = useState([]);
+  // const [data, setData] = useState([]);
   const [weatherData, setWeatherData] = useState([]);
   const [error, setError] = useState(null);
 
@@ -14,6 +14,7 @@ function App() {
       setLat(position.coords.latitude);
       setLong(position.coords.longitude);
     });
+
     getWeather(lat, long)
       .then(weather => {
         setWeatherData(weather);
@@ -22,14 +23,54 @@ function App() {
         setError(err.message);
       });
 
-  }, [lat, long])
+    function getWeather(lat, long) {
+      return fetch(
+        `${process.env.REACT_APP_API_URL}/weather/?lat=${lat}&lon=${long}&units=metric&APPID=${process.env.REACT_APP_API_KEY}`
+      )
+        .then(res => handleResponse(res))
+        .then(weather => {
+          if (Object.entries(weather).length) {
+            const mappedData = mapDataToWeatherInterface(weather);
+            return mappedData;
+          }
+        });
+    }
+
+  }, [lat, long, error])
+
+  function handleResponse(response) {
+    if (response.ok) {
+      return response.json();
+    } else {
+      throw new Error("Please Enable your Location in your browser!");
+    }
+  }
+
+  console.log(`Latitude: ${lat}`);
+  console.log(`Longitude: ${long}`);
+
+  function mapDataToWeatherInterface(data) {
+    const mapped = {
+      date: data.dt * 1000, // convert from seconds to milliseconds
+      description: data.weather[0].main,
+      temperature: Math.round(data.main.temp),
+    };
+
+    // Add extra properties for the five day forecast: dt_txt, icon, min, max
+    if (data.dt_txt) {
+      mapped.dt_txt = data.dt_txt;
+    }
+
+    return mapped;
+  }
 
   return (
     <div>
-      {(typeof data.main != 'undefined') ? (
-        <Main weatherData={data} />
+      {(typeof weatherData != 'undefined') ? (
+        <Main weatherData={weatherData} />
       ) : (
-        <div>
+        <div className='flex flex-col items-center w-full h-screen justify-center text-2xl'>
+          <p>Loading...</p>
         </div>
       )}
     </div>
