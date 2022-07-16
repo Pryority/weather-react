6f618be6c43ec67470645c687979bcd7c5e95184
pryority <matthewapryor@gmail.com>
2022-07-16 11:47:15 -0400

    add getWeather function

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import Main from './components/Main'
 
 function App() {
+  const [lat, setLat] = useState([]);
+  const [long, setLong] = useState([]);
+  const [data, setData] = useState([]);
+  const [weatherData, setWeatherData] = useState([]);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(function (position) {
+      setLat(position.coords.latitude);
+      setLong(position.coords.longitude);
+    });
+    getWeather(lat, long)
+      .then(weather => {
+        setWeatherData(weather);
+        setError(null)
+      }).catch(err => {
+        setError(err.message);
+      });
+
+  }, [lat, long])
+
   return (
-    <div id='wrapper' className="flex flex-col w-full items-center">
-      <div className='flex w-full bg-slate-300 justify-center p-2'>
-        <h1>Weather Dashboard</h1>
-      </div>
-      <div className='flex flex-col items-center bg-red-100 h-screen p-2'>
+    <div>
+      {(typeof data.main != 'undefined') ? (
+        <Main weatherData={data} />
+      ) : (
         <div>
-          <h3>Search for a City:</h3>
         </div>
-        <div>
-          <input placeholder='Toronto' />
-        </div>
-        <div>
-          <button>Search</button>
-        </div>
-        <div className='flex w-full border border-slate-300' />
-      </div>
+      )}
     </div>
   );
 }
---
+++ src/components/Main.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+export default function Main({ weatherData }) {
+    return (
+        <div id='wrapper' className="flex flex-col w-full items-center">
+            <div className='flex w-full bg-slate-300 justify-center p-2'>
+                <h1>Weather Dashboard</h1>
+            </div>
+            <div className='flex flex-col items-center bg-red-100 h-screen p-2'>
+                <div>
+                    <h3>Search for a City:</h3>
+                </div>
+                <div>
+                    <input placeholder='Toronto' />
+                </div>
+                <div>
+                    <button>Search</button>
+                </div>
+                <div className='flex w-full border border-slate-300' />
+                <p>{weatherData.name}</p>
+            </div>
+        </div>
+    )
+}
